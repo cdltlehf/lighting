5358846433febb0be9bc8535cc4d5931b32cb46f
Sicheol Sung <cdltlehf@gmail.com>
2026-01-08 17:12:59 +0900

    feat: Add dithering feature

--- src/main.rs
+++ src/main.rs
@@ -46,18 +46,22 @@ struct Args {
     inner_angle_factor: f32,
     #[clap(long, default_value = "contain")]
     object_fit: ObjectFit,
+
+    #[clap(long, default_value_t = 0.0)]
+    dithering: f32,
 }
 
 fn main() {
     let args = Args::parse();
     let Args {
-        width,
+        dithering,
         height,
-        temperature,
+        inner_angle_factor,
         intensity,
         outer_angle,
-        inner_angle_factor,
         output,
+        temperature,
+        width,
         ..
     } = args;
     let light_pos = (args.light_pos_x, args.light_pos_y, args.light_pos_z);
@@ -76,6 +80,7 @@ fn main() {
         let rgb = rgb_from_temperature(temperature);
         Srgb::from_components(rgb.into_components()).into_format::<f32>()
     };
+
     let inner_cos = inner_angle.cos();
     let outer_cos = outer_angle.cos();
 
@@ -84,7 +89,6 @@ fn main() {
     img.par_chunks_mut(4).enumerate().for_each(|(i, pixel)| {
         let x_idx = (i as u32) % width;
         let y_idx = (i as u32) / width;
-
         let (x, y) = {
             let (x, y) = (
                 x_idx as f32 / width as f32 * 2.0 - 1.0,
@@ -98,6 +102,8 @@ fn main() {
         };
         let z = 0.0;
 
+        let dithering = (random(x, y) * 2.0 - 1.0) * dithering;
+
         let (dx, dy, dz) = (x - light_pos.0, y - light_pos.1, z - light_pos.2);
         let distance = (dx * dx + dy * dy + dz * dz).sqrt();
         let (lx, ly, lz) = (-dx / distance, -dy / distance, -dz / distance);
@@ -110,7 +116,9 @@ fn main() {
         let diffusion = intensity * (lx * nx + ly * ny + lz * nz).max(0.0);
         let lin_color = LinSrgb::from_color(temp_color) * diffusion;
 
-        let color = Srgb::from_color(lin_color).clamp().into_format::<u16>();
+        let color = Srgb::from_color(lin_color + dithering)
+            .clamp()
+            .into_format::<u16>();
         pixel[0] = color.red;
         pixel[1] = color.green;
         pixel[2] = color.blue;
@@ -123,4 +131,8 @@ fn main() {
 fn smoothstep(edge0: f32, edge1: f32, x: f32) -> f32 {
     let t = ((x - edge0) / (edge1 - edge0)).clamp(0.0, 1.0);
     t * t * (3.0 - 2.0 * t)
+}
+
+fn random(x: f32, y: f32) -> f32 {
+    return ((x * 12.9898 + y * 78.233) * 43758.5453123).sin().fract();
 }
